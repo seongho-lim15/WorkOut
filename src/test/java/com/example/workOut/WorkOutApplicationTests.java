fb6ff622a01651da11ccc9bda59ddfd83de3c3f1
Wokwok2 <hws0602@naver.com>
2022-11-11 16:31:13 +0900

    첫 커밋

---
+++ src/test/java/com/example/workOut/WorkOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
