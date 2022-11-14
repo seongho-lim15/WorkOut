1b008b407c489d381ccaaa58bd0703feddf87922
Wokwok2 <hws0602@naver.com>
2022-11-14 16:38:40 +0900

    달력 기능 추가중

--- src/main/java/com/example/workOut/controller/WorkOutController.java
+++ src/main/java/com/example/workOut/controller/WorkOutController.java
@@ -1,13 +1,25 @@
 package com.example.workOut.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.workOut.domain.DayOfCalendar;
 
 @Controller
 public class WorkOutController {
 
 	@GetMapping("workout")
-	public String workOut() {
+	public String workOut(Model model) {
+		
+		DayOfCalendar DOC = new DayOfCalendar();
+		
+		List<Integer>list = DOC.prn(2022, 11);
+		
+		model.addAttribute("list",list);
+		
 		return "workout";
 	}
 }
