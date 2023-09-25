f84fbe705ef1375115dccf554d8fa16d36d57917
applzz <hws0602@naver.com>
2023-09-25 20:21:52 +0900

    230925

--- src/main/java/com/example/workOut/controller/WorkOutController.java
+++ src/main/java/com/example/workOut/controller/WorkOutController.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 import java.util.logging.Logger;
 
 import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +20,10 @@ import org.springframework.web.bind.annotation.*;
 import com.example.workOut.domain.DayOfCalendar;
 
 @Controller
-@RequestMapping("workout")
+@RequestMapping("/workout")
 public class WorkOutController {
 
-	@GetMapping("workout")
+	@GetMapping("/")
 	public String workOut(Model model) {
 
 		DayOfCalendar dc = new DayOfCalendar();
@@ -45,37 +46,36 @@ public class WorkOutController {
 	}
 	// 팝업 요청 시
 	@GetMapping("/input")
-	public String workoutPopUp(@RequestParam("day")String day,
-							   @RequestParam("year")String year,
-							   @RequestParam("month")String month,
+	public String workoutPopUp(@RequestParam("date") String date,
 							   Model model) {
 
-		Map<String, Object> date = new HashMap<>();
-
-		date.put("day", day);
-		date.put("year", year);
-		date.put("month", month);
-
+//		Map<String, Object> date = new HashMap<>();
+//		date.put("date", date);
 		// Gson 객체 생성
-		Gson gson = new Gson();
-		String jsonDate = gson.toJson(date);
+//		Gson gson = new Gson();
+//		String jsonDate = gson.toJson(date);
 //
 //		System.out.println("jsonDate = " + jsonDate);
 		// 받아온 날짜 데이터들을 date map 에 넣어서 팝업 화면에 응답으로 보냄
-		model.addAttribute("date",jsonDate);
+		model.addAttribute("date",date);
 		return "workoutPopup";
 	}
 
 	@PostMapping ("/save-exercise")
 	public ResponseEntity<Object> saveWorkout(@RequestBody Map<String,Object> param) {
 
+		System.out.println("param: "+param);
+
 		// exercise 에 값이 존재한다면
 		if(param.get("exercise") != null){
 			// header 생성
-			HttpHeaders headers= new HttpHeaders();
-			headers.add("Custom-Header", "foo");
+//			HttpHeaders headers= new HttpHeaders();
+//			headers.add("Custom-Header", "foo");
 
-			return ResponseEntity.ok().headers(headers).body(param);
+			return ResponseEntity
+					.ok()
+//					.headers(headers)
+					.body(param);
 		}else{
 			return ResponseEntity.badRequest().body("error 이다.");
 		}
--- src/main/resources/static/js/popup.js
+++ src/main/resources/static/js/popup.js
@@ -1,3 +1,4 @@
 function popup(date) {
-    window.open("workout/input?day="+date.day+'&year='+date.year+'&month='+date.month, "pop", "width=570, height=420, scrollbars=yes");
+    window.open("/workout/input?date="+date.year+'-'+date.month+'-'+date.day, "pop", "width=570, height=420, scrollbars=yes");
+    // window.open("/workout/input?day="+date.day+'&year='+date.year+'&month='+date.month, "pop", "width=570, height=420, scrollbars=yes");
 }
